4fa75009a618cf88c0cfdc637c5c7fe9f5142381
manuntag <manuntag@ualberta.ca>
2015-01-26 11:06:15 -0800

    Initial Commit

---
+++ BillSplitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillSplitter
+//
+//  Created by David Manuntag on 2015-01-26.
+//  Copyright (c) 2015 David Manuntag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
